d66f80b00278933a0f3ca29b6ac6cbf563778b6a
EC2 Default User <ec2-user@ip-172-31-16-130.eu-west-1.compute.internal>
2019-01-03 13:01:22 +0000

    adding debug log of the event passed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
+const util = require('util')
 
 var fs = require('fs');
 var path = require('path');
 
 exports.get = function(event, context, callback) {
+  console.log(util.inspect(event, {showHidden: false, depth: null}))
   var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
     statusCode: 200,
